c48544e3ba4f1ffc97ab9762991a598260be04fe
David Karlaš <david.karlas@gmail.com>
2020-02-13 14:09:31 +0100

    bla 3

--- TodoWithRoutes/Todo.cs
+++ TodoWithRoutes/Todo.cs
@@ -7,6 +7,9 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
+        /// <summary>
+        /// Check if task is completed.
+        /// </summary>
         public bool IsComplete { get; set; }
     }
 }
